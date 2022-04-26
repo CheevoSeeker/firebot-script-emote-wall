307221213a5ec90ee7c94db9207de04a64a78e53
CheevoSeeker <git@cheevo.link>
2022-04-25 21:07:11 -0400

    First version of the Emote Wall script

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-# Starter Firebot Custom Script in Typescript
+# Emote Wall Firebot Script
+
+## How to use
+1. Download the latest **emoteWall.js** file from [Releases](https://github.com/CheevoSeeker/firebot-script-emote-wall/releases)
+2. Copy the **emoteWall.js** file to Firebot's scripts folder
+3. Add a `Chat Message (Twitch)` event
+    - Add a `Conditional Effects` effect
+        - If `$textLength[$chatMessageEmoteUrls]` is greater than `0`
+            - Add a `Run Custom Script` effect
+                - Select `emoteWall.js`
+                - Set the various options
+
+## Developers
 
 ### Setup
-1. Create a new repo based off this template (Click "Use this Template" above) or simply fork it
+1. Clone or fork this repository
 2. `npm install`
 
 ### Building
 Dev:
 1. `npm run build:dev`
-- Automatically copies the compiled .js to Firebot's scripts folder.
+    - Automatically copies the compiled `emoteWall.js` to Firebot's `scripts` folder.
 
 Release:
 1. `npm run build`
-- Copy .js from `/dist`
-
-### Note
-- Keep the script definition object (that contains the `run`, `getScriptManifest`, and `getDefaultParameters` funcs) in the `index.ts` file as it's important those function names don't get minimized.
-- Edit the `"scriptOutputName"` property in `package.json` to change the filename of the outputted script.
+2. Copy the `emoteWall.js` file in `/dist` to Firebot's `scripts` folder
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,93 @@
 import { Firebot } from "firebot-custom-scripts-types";
 
 interface Params {
-  message: string;
+  emoteUrls: string;
+  emoteSize: number;
+  overlayWidth: number;
+  overlayHeight: number;
 }
 
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
     return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
+      name: "Emote Wall",
+      description: "Shows chat emotes on screen",
+      author: "CheevoSeeker",
       version: "1.0",
-      firebotVersion: "5",
+      website: "https://github.com/CheevoSeeker/firebot-script-emote-wall",
+      startupOnly: false,
+      firebotVersion: "5"
     };
   },
   getDefaultParameters: () => {
     return {
-      message: {
+      emoteUrls: {
         type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
+        description: "URLs of the emotes to show",
+        default: "$chatMessageEmoteUrls"
+      },
+      emoteSize: {
+        type: "number",
+        description: "Size of the emotes",
+        default: 64
+      },
+      overlayWidth: {
+        type: "number",
+        description: "Width of the overlay",
+        default: 1920
+      },
+      overlayHeight: {
+        type: "number",
+        description: "Height of the overlay",
+        default: 1080
       },
     };
   },
-  run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
+  run: ({ parameters, modules }) => {
+    const { logger } = modules;
+
+    // Parse parameters
+    const { emoteUrls, emoteSize, overlayWidth, overlayHeight } = parameters;
+    const emoteUrlArr = emoteUrls.split(",");
+    const randomId = "CheevoEmoteWall_" + Date.now();
+    if (emoteUrlArr.length <= 0) { return; }
+
+    // Define helper functions
+    let randomPosition = function(max: number) {
+      return Math.floor(Math.random() * max) - emoteSize;
+    };
+
+    // Generate HTML and JavaScript
+    let imageTags = [];
+    let scriptLines = [];
+    for (let index = 0; index < emoteUrlArr.length; index++) {
+      const emoteUrl = emoteUrlArr[index];
+      const emoteId = `${randomId}_${index}`;
+
+      const startStyle = `position: absolute; left: ${randomPosition(overlayWidth)}px; top: ${randomPosition(overlayHeight)}px;`;
+      imageTags.push(`<img class="${emoteId}" src="${emoteUrl}" width="${emoteSize}" style="${startStyle}">`);
+
+      const endStyle = `{left: ${randomPosition(overlayWidth)}, top: ${randomPosition(overlayHeight)}}`;
+      scriptLines.push(`$(".${emoteId}").animate(${endStyle}, 3000);`);
+    }
+
+    // Return HTML effect
+    let htmlContent = `<div class="${randomId}">${imageTags.join("")}<script>${scriptLines.join("")}</script></div>`;
+    return {
+      success: true,
+      errorMessage: "Failed to run the script!",
+      effects: [
+        {
+          type: "firebot:html",
+          html: htmlContent,
+          length: 1,
+          enterAnimation: "fadeIn",
+          enterDuration: 1,
+          exitAnimation: "fadeOut",
+          exitDuration: 1
+        }
+      ]
+    };
   },
 };
 
